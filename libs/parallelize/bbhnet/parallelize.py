0986bcf926cf692d216964d2e9e3e8668a2e2ada
Alec Gunny <alec.gunny@gmail.com>
2022-04-29 10:48:42 -0700

    refactoring parallelization

--- libs/parallelize/bbhnet/parallelize.py
+++ libs/parallelize/bbhnet/parallelize.py
@@ -6,9 +6,12 @@ from typing import Callable
 from bbhnet.io.timeslides import Segment
 
 
-def serial_gen(f, segments, *args, **kwargs):
-    for segment in segments:
-        yield f(segment, *args, **kwargs)
+class ProcessPool:
+    def __init__(self, num_proc):
+        self.executor = ProcessPoolExecutor(num_proc)
+
+    def parallelize(self, f):
+        return segment_iterator(f, self)
 
 
 def async_gen(executor, futures):
@@ -19,25 +22,18 @@ def async_gen(executor, futures):
                 raise exc
 
             yield future.result()
-    finally:
+    except Exception:
         executor.shutdown(wait=False, cancel_futures=True)
+        raise
 
 
-def segment_iterator(f: Callable) -> Callable:
+def segment_iterator(f: Callable, ex: ProcessPool) -> Callable:
     """
     Function wrapper that can parallelize a function across multiple segments
     """
 
     @wraps(f)
     def wrapper(*args, **kwargs):
-        # first check if a number of processes was specified,
-        # and pop it out of the kwargs since num_proc shouldn't
-        # be an argument of the function itself
-        try:
-            num_proc = kwargs.pop("num_proc")
-        except KeyError:
-            num_proc = None
-
         # get the name of the first argument, which should
         # be either a segment or a list of segments.
         # TODO: enforce naming or annotation constrictions here?
@@ -52,27 +48,9 @@ def segment_iterator(f: Callable) -> Callable:
             args = args[1:]
 
         if isinstance(segments, Segment):
-            # if this is just a single segment, then just call
-            # the function as-is on the single segment
-            if num_proc is not None:
-                raise ValueError(
-                    "'num_proc' can't be specified if {} "
-                    "is a single Segment".format(arg0)
-                )
             return f(segments, *args, **kwargs)
-        elif num_proc is None or num_proc == 1:
-            # otherwise if we have multiple segments but didn't
-            # specify multiprocessing, just iterate through the
-            # function's output on the segments
-            # TODO: some sort of type checking to ensure that
-            # this is an iterable of segments?
-            return serial_gen(f, segments, *args, **kwargs)
         else:
-            # last case is that we have multiple segments _and_
-            # we specified multiprocessing. So start up a process
-            # pool and submit the function and various segments to
-            # the pool for async processing
-            ex = ProcessPoolExecutor(num_proc)
+            # if we have multiple, analyze them in parallel
             futures = [
                 ex.submit(f, segment, *args, **kwargs) for segment in segments
             ]
