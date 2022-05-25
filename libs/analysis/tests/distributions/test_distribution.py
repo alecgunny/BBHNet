77e44e8fd3cdaf0cd1d8a4ec7506bcc286fc338d
Alec Gunny <alec.gunny@gmail.com>
2022-05-25 12:23:22 -0700

    finishing distribution test

--- libs/analysis/tests/distributions/test_distribution.py
+++ libs/analysis/tests/distributions/test_distribution.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from math import exp
 from unittest.mock import MagicMock, Mock
 
@@ -8,6 +9,7 @@ from bbhnet.analysis.distributions.distribution import (
     SECONDS_IN_YEAR,
     Distribution,
 )
+from bbhnet.io.timeslides import Segment
 
 
 def nb(x):
@@ -45,24 +47,34 @@ def test_distribution(event_time, event_times, offset, sample_rate):
     distribution.nb = nb
     distribution.Tb = SECONDS_IN_YEAR / 2
 
+    # unit test characterization methods
+    # false alarm rate
     far = distribution.far(0)
     assert far == 20
 
+    # significance
     tf = (event_time + offset) * 2
     significance = distribution.significance(0, tf)
     assert significance == 1 - exp(-tf * 22 / SECONDS_IN_YEAR)
 
+    # now create some dummy event segments and ensure
+    # that the characterization curves that come out
+    # of them match up
     t = np.arange(0, tf, 1 / sample_rate)
     y = np.ones_like(t)
-    segment = Mock()
+    segment = Mock(Segment)
     segment.load = MagicMock(return_value=(y, t))
 
     for metric, expected in zip(["far", "significance"], [far, significance]):
         characterization, times = distribution.characterize_events(
             segment, event_times=event_times, window_length=1, metric=metric
         )
+
+        # make sure the appropriate segment data got "loaded"
         segment.load.assert_called_with("test")
 
+        # for a single event, make sure we have 1D
+        # timeseries of the appropriate length and content
         if isinstance(event_times, float):
             assert characterization.ndim == 1
             assert len(characterization) == sample_rate
@@ -75,6 +87,8 @@ def test_distribution(event_time, event_times, offset, sample_rate):
             t_expect = t[start : start + sample_rate] - event_time
             assert np.isclose(times, t_expect, rtol=1e-9).all()
         else:
+            # for 2D, make sure we have 3 events with the right
+            # length and content
             assert characterization.shape == (3, sample_rate)
             assert times.shape == (3, sample_rate)
 
@@ -91,4 +105,39 @@ def test_distribution(event_time, event_times, offset, sample_rate):
                 t_expect = t[start : start + sample_rate] - tc
                 assert np.isclose(times[i], t_expect, rtol=1e-9).all()
 
+        # characterization values will be constant since
+        # that's how we've set up `nb` to return things
         assert np.isclose(characterization, expected, rtol=1e-7).all()
+
+    # override the update method to make sure
+    # it gets called appropriately and the right
+    # number of times TODO: a MagicMock could
+    # probably do this just as well
+    def update(*args):
+        distribution.Tb += 10
+
+    distribution.update = update
+
+    # test fitting on a single segment
+    segment.fnames = ["test1"]
+    distribution.fit(segment)
+
+    # fnames are right, backround time updated
+    assert distribution.fnames == ["test1"]
+    assert distribution.Tb == SECONDS_IN_YEAR / 2 + 10
+
+    # now create a list of segments to fit on
+    segment2 = deepcopy(segment)
+    segment.fnames = ["test2", "test3"]
+    segment2.fnames = ["test4"]
+    distribution.fit([segment, segment2])
+
+    # same checks
+    assert distribution.fnames == [f"test{i + 1}" for i in range(4)]
+    assert distribution.Tb == SECONDS_IN_YEAR / 2 + 30
+
+    # now fit on the list but without warm starting,
+    # which should reset `fnames` and `Tb`
+    distribution.fit([segment, segment2], warm_start=False)
+    assert distribution.fnames == [f"test{i + 2}" for i in range(3)]
+    assert distribution.Tb == 20
