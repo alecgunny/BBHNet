d5aba8182792a54939098d4266a2ce1e84e0f5ea
Alec Gunny <alec.gunny@gmail.com>
2023-03-20 11:43:08 -0700

    getting deployment script working

--- projects/sandbox/infer/infer/deploy.py
+++ projects/sandbox/infer/infer/deploy.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import re
 import shutil
@@ -9,6 +10,7 @@ from typing import List
 
 from typeo import scriptify
 
+from bbhnet.logging import configure_logging
 from hermes.aeriel.serve import serve
 
 re_dagman_cluster = re.compile(r"(?<=submitted\sto\scluster )[0-9]+")
@@ -39,17 +41,17 @@ def calc_shifts_required(Tb: float, T: float, delta: float) -> int:
 def get_num_shifts(data_dir: Path, Tb: float, shift: float) -> int:
     T = 0
     for fname in data_dir.iterdir():
-        match = re_fname.search(fname)
+        match = re_fname.search(fname.name)
         if match is not None:
-            duration = match.group(1)
-            T += duration
+            duration = match.group(2)
+            T += float(duration)
     return calc_shifts_required(Tb, T, shift)
 
 
-def get_exectubale(name: str) -> str:
+def get_executable(name: str) -> str:
     ex = shutil.which(name)
     if ex is None:
-        raise ValueError(f"No executable {ex}")
+        raise ValueError(f"No executable {name}")
     return str(ex)
 
 
@@ -79,9 +81,9 @@ def create_submit_file(
         getenv = True
         accounting_group = {accounting_group}
         accounting_group_user = {accounting_group_user}
-        request_memory = 1024
-        request_disk = 1024
-        queue shifts,seq_id from {condor_dir}/shifts.txt
+        request_memory = 6G
+        request_disk = 1G
+        queue shift0,shift1,seq_id from {condor_dir}/shifts.txt
     """
     )
     return subfile
@@ -112,23 +114,25 @@ def main(
     model_version: int = -1,
     verbose: bool = False,
 ):
+    configure_logging(output_dir / "infer.deploy.log", verbose)
+
     # get ip address and add to arguments
     # along with the timeslide datadir which will be read from a text file
-    ip = get_ip_address()
+    # ip = get_ip_address()
     log_pattern = "infer-$(ProcID).log"
     output_pattern = "tmp/output-$(ProcID)"
     arguments = f"""
-    --data-dir ${data_dir}
-    --output_dir {output_dir / output_pattern}
-    --shifts $(shifts)
+    --data-dir {data_dir}
+    --output-dir {output_dir / output_pattern}
+    --shifts $(shift0) $(shift1)
     --sequence-id $(seq_id)
     --log-file {output_dir / log_pattern}
-    --ip {ip}
+    --ip 10.12.3.11
     --model-name {model_name}
     --injection-set-file {injection_set_file}
     --sample-rate {sample_rate}
     --inference-sampling-rate {inference_sampling_rate}
-    --ifos {ifos}
+    --ifos {" ".join(ifos)}
     --batch-size {batch_size}
     --integration-window-length {integration_window_length}
     --cluster-window-length {cluster_window_length}
@@ -143,7 +147,12 @@ def main(
 
     condor_dir = output_dir / "condor"
     condor_dir.mkdir(exist_ok=True, parents=True)
-    executable = shutil.which("infer")
+
+    import os
+
+    bin_dir = "/home/alec.gunny/miniconda3/envs/infer-N6p8Fb-q-py3.9/bin"
+    os.environ["PATH"] = bin_dir + ":" + os.environ["PATH"]
+    executable = get_executable("infer")
     subfile = create_submit_file(
         executable,
         condor_dir,
@@ -157,26 +166,25 @@ def main(
     with open(subfile_path, "w") as f:
         f.write(subfile)
 
-    num_shifts = get_num_shifts(data_dir, Tb)
+    num_shifts = get_num_shifts(data_dir, Tb, shift)
+    logging.info(f"Submitting jobs for {num_shifts} shifts")
     with open(condor_dir / "shifts.txt", "w") as f:
         for i in range(num_shifts):
             seq_id = sequence_id + 2 * i
-            f.write(f"0 {i * shift},{seq_id}")
+            f.write(f"0,{i * shift},{seq_id}\n")
 
-    condor_submit = shutil.which("condor_submit")
-    if condor_submit is None:
-        raise ValueError("No condor_submit executable")
-
+    condor_submit = get_executable("condor_submit")
+    cwq = get_executable("condor_watch_q")
     cmd = [condor_submit, str(subfile_path)]
 
     # spin up triton server
+    logging.info("Launching triton server")
     with serve(model_repo, image, wait=True):
         # launch inference jobs via condor
         out = subprocess.check_output(cmd, text=True)
 
         # find dagman id and wait for jobs to finish
         dagid = int(re_dagman_cluster.search(out).group())
-        cwq = shutil.which("condor_watch_q")
         subprocess.check_call(
             [
                 cwq,
