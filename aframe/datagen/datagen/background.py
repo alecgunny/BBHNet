975a5af40998d4d7002a89b4409b4443bd2c9617
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-08-30 09:13:55 -0700

    add executables for querying segments and background data

---
+++ aframe/datagen/datagen/background.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from typing import List
+
+from typeo import scriptify
+
+from mldatafind.authenticate import authenticate
+from mldatafind.io import fetch_timeseries
+
+
+@scriptify
+def main(
+    start: float,
+    stop: float,
+    channel: str,
+    ifos: List[str],
+    sample_rate: float,
+    write_path: Path,
+):
+    """Generates background data for training and testing aframe
+
+    Args:
+        start:
+            Starting GPS time of the timeseries to be fetched
+        stop:
+            Ending GPS time of the timeseries to be fetched
+        writepath:
+            Path, including file name, that the data will be saved to
+        channel:
+            Channel from which to fetch the timeseries
+        ifos:
+            List of interferometers to query data from. Expected to be given
+            by prefix; e.g. "H1" for Hanford
+        sample_rate:
+            Sample rate to which the timeseries will be resampled, specified
+            in Hz
+
+    Returns: The `Path` of the output file
+    """
+    authenticate()
+    channels = [f"{ifo}:{channel}" for ifo in ifos]
+    data = fetch_timeseries(channels, start, stop)
+    data = data.resample(sample_rate)
+    for ifo in ifos:
+        data[ifo] = data.pop(f"{ifo}:{channel}")
+
+    data.write(write_path)
+    return write_path
---
+++ aframe/datagen/datagen/segments.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import List
+
+import numpy as np
+from typeo import scriptify
+
+from mldatafind.segments import query_segments
+
+
+@scriptify
+def main(
+    start: float,
+    stop: float,
+    state_flag: str,
+    minimum_length: float,
+    ifos: List[str],
+    data_dir: Path,
+):
+    """Wrapper around query_segments to write segments
+    to a file for compatibility with Luigi Tasks
+    """
+
+    segments = query_segments(
+        [f"{ifo}:{state_flag}" for ifo in ifos],
+        start,
+        stop,
+        minimum_length,
+    )
+
+    path = data_dir / "segments.txt"
+    np.savetxt(
+        path,
+        segments,
+    )
+    return path
